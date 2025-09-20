4e6deb8ac37ac7cd96f7ca8ba81e5a1f2d9e5e40
abhijittechniciandey <abhijittechniciandey@gmail.com>
2025-09-20 09:10:11 +0530

    third commit

--- Revision/TemplateTaxI/src/main/java/com/example/tax/PropertyTax.java
+++ Revision/TemplateTaxI/src/main/java/com/example/tax/PropertyTax.java
@@ -1,6 +1,54 @@
 package com.example.tax;
 
-public class PropertyTax {
+public class PropertyTax implements Tax{
+	
+	double taxableAmount;
+	double taxAmount;
+	boolean isTaxpayed=false;
+
+	@Override
+	public void setTaxableAmount(int amount) {
+		// TODO Auto-generated method stub
+		this.taxableAmount=amount;
+		
+	}
+
+	@Override
+	public void calculateTaxAmount() {
+		// TODO Auto-generated method stub
+		if (!isTaxpayed) {
+            taxAmount = taxableAmount * 0.05;
+        }		
+	}
+
+	@Override
+	public double getTaxAmount() {
+		// TODO Auto-generated method stub
+		return taxAmount;
+	}
+
+	@Override
+	public String getTaxType() {
+		// TODO Auto-generated method stub
+		return "property";
+	}
+
+	@Override
+	public boolean isTaxPayed() {
+		// TODO Auto-generated method stub
+		return isTaxpayed;
+	}
+
+	@Override
+	public void payTax() {
+		// TODO Auto-generated method stub
+		if (!isTaxpayed) {
+            isTaxpayed = true;
+            System.out.println("Hi, your property tax is paid.");
+        } else {
+            System.out.println("Property Tax is already paid.");
+        }	
+	}
     /*
     1. Create the following attributes.
         a. taxableAmount (double)
