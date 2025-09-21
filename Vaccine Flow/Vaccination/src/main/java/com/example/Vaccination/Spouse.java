9780901c7a0df7e0d2b65615ea5350a9d7d516b1
abhijittechniciandey <abhijittechniciandey@gmail.com>
2025-09-21 23:15:34 +0530

    Vaccine Problem I

---
+++ Vaccine Flow/Vaccination/src/main/java/com/example/Vaccination/Spouse.java
@@ -0,0 +1,40 @@
+package com.example.Vaccination;
+
+public class Spouse implements User {
+
+    private String name;
+    private Integer age;
+    private TimeAndLocation timeAndLocation;
+    private Vaccine vaccine;
+    private boolean isVaccinated = false;
+
+    public Spouse( TimeAndLocation timeAndLocation, Vaccine vaccine){
+        this.timeAndLocation = timeAndLocation;
+        this.vaccine = vaccine;
+    }
+
+    @Override
+    public void setAppointment() {
+        this.isVaccinated = true;
+        String vaccineDetails = vaccine.getType();
+        System.out.println("Hello " + name + " your appointment has been fixed for " + vaccineDetails
+                + " Vaccine on " + timeAndLocation.getDetails());
+    }
+
+    @Override
+    public Vaccine getVaccineDetails() {
+        return this.vaccine;
+    }
+
+    @Override
+    public void setUserDetails(String name, int age, TimeAndLocation timeAndLocation) {
+        this.name = name;
+        this.age = age;
+        this.timeAndLocation = timeAndLocation;
+    }
+
+    @Override
+    public boolean IsVaccinated() {
+        return this.isVaccinated;
+    }
+}
---
+++ Vaccine Flow/Vaccination/src/main/java/com/example/Vaccination/Typhoid.java
@@ -0,0 +1,9 @@
+package com.example.Vaccination;
+
+public class Typhoid implements Vaccine {
+
+    @Override
+    public String getType(){
+        return "Typhoid";
+    }
+}
