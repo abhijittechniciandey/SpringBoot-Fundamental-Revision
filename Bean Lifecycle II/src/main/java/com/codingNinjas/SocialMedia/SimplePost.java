3be00fdb0c96d24b15c56d5b954819972888ba86
abhijittechniciandey <abhijittechniciandey@gmail.com>
2025-09-27 22:58:25 +0530

    commit

---
+++ Bean Lifecycle II/src/main/java/com/codingNinjas/SocialMedia/SimplePost.java
@@ -0,0 +1,21 @@
+package com.codingNinjas.SocialMedia;
+
+public class SimplePost implements Post {
+	
+	String message;
+	
+	public void destroy() {
+		System.out.println("Post destroyed");
+	}
+
+	@Override
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String getMessage() {
+		return this.message;
+	}
+
+}
